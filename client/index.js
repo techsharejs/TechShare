465c0f1967c29838bede2be68bedd9d252773676
jeff <kimjeffrey96@gmail.com>
2018-10-22 22:01:38 -0400

    basic redux up

--- client/index.js
+++ client/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App.jsx';
 
+// const store = createStore();
+
 ReactDOM.render(
-  <App />,
+  // <Provider store={store}>
+    <App />,
+  // </Provider>,
   document.getElementById('content'),
 );
---
+++ client/modules/users/actions/actions.js
@@ -0,0 +1,9 @@
+/**
+ * @module actions.js
+ * @author
+ * @description Action Creators
+ */
+
+ import * as types from '';
+
+ export default 
