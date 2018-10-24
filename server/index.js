16d5de5f3b9205055f04e0bdc82f1afb2a4f30e8
james-owen <james@jamesowen.io>
2018-10-24 15:17:48 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const db = require('./queries');
 
 const PORT = 8080;
 
-app.get('/', db.getCategory);
+app.use(express.static(path.join(__dirname + './../')));
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname + './../public/index.html'));
+}, db.getCategory);
 
 app.listen(PORT, () => {
   console.log(`Listening to PORT: ${PORT}`);
